4c346698984f42eda3ab86e3e2b041b6250b4e20
Lizzie Siegle <esiegle@brynmawr.edu>
2021-04-14 18:04:32 -0700

    disclaimer

--- functions/haiku.js
+++ functions/haiku.js
@@ -1,5 +1,23 @@
 let syllLib = require('syllable');
 const superagent = require('superagent');
+function checkForAndSendDislaimer(client, sendTo, sendFrom) {
+  const disclaimer = 'Thanks for texting! This bot uses Twilio Programmable Messaging for handling text messages, and Datamuse to generate words. If you want to learn more about the technology behind this, check out \n\nwww.datamuse.com/api\n\nGeneral Content Warning:\n\nThis bot pulls words according to number of syllables from a massive corpus of text, which means that the text it generates may reflect the biases of society at large. There is a chance that your response may contain language that is harmful.\n\nIf there are any problems, you can reach out to the developer behind the bot, @lizziepika on Twitter, or Datamuse\'s team directly.'
+  const filter = {
+      to: sendFrom,
+      from: sendTo,
+      dateSentAfter: new Date(Date.UTC(2021, 4, 14))
+  }
+  client.messages.list(filter)
+    .then(messages => {
+      if(messages.length == 1) {
+        client.messages.create({
+          body: disclaimer,
+          to: sendTo,
+          from: sendFrom,
+        })
+      }
+    });
+}
 function makeLine (numSyllables, wordPool) { //Func generates 3 lines according to input (haiku lines have syllables of # 5, 7, 5; generate words from list of words relating to input word)
   if (numSyllables == 0 || numSyllables != parseInt(numSyllables, 10)) {
     numSyllables = 5;
@@ -17,6 +35,9 @@ function makeLine (numSyllables, wordPool) { //Func generates 3 lines according 
   return line.trim();
 };
 exports.handler = async function(context, event, callback) {
+  const twilioClient = context.getTwilioClient()
+  // If this number hasn't texted before, send them the disclaimer and blog link.
+  checkForAndSendDislaimer(twilioClient, event.From, event.To)
   let twiml = new Twilio.twiml.MessagingResponse();
   let inbMsg = event.Body.toLowerCase().trim();
   if(inbMsg.slice(-1) == "s") {
